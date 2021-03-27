f2ca75655b44c592056c26ff50fb979d103a6c7d
mcdogg17 <makdog45@mail.ru>
2021-03-27 23:37:56 +0500

    autoform

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from werkzeug.utils import redirect
 from data.kgs import KGS
 
+
+class AuthorizationForm(FlaskForm):
+    login = StringField('Логин:', validators=[DataRequired()])
+    password = PasswordField('Пароль: ', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pythonidory'
 api = KGS("login", "password", "ru_RU")
 
 
 @app.route('/')
+@app.route('/authorization')
+def authorization():
+    form = AuthorizationForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template('index.html', title='Авторизация', form=form)
+
+
 @app.route('/index')
 def index():
     return render_template('table.html', users=api.parse_top_100())
