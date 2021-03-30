89db85997305819a837848585b528ded8a5a0d8e
Ilushandr <ilushandr@gmail.com>
2021-03-30 22:57:28 +0500

    css commit

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import json
-
 from flask import Flask, render_template, request, jsonify
 from data.kgs import KGS
 
 app = Flask(__name__)
-api = KGS("login", "password", "ru_RU")
+api = KGS("pamparam", "i7deu8", "ru_RU")
+REQUESTED = []
 
 
 @app.route('/')
@@ -16,26 +15,33 @@ def index():
 @app.route('/info', methods=['POST'])
 def get_user_info():
     user = request.form['user_name']
-    arh_join = KGS.get_typed(api.join_archive_request(user)["messages"], "ARCHIVE_JOIN")
+    if user not in REQUESTED:
+        REQUESTED.append(user)
+        arh_join = KGS.get_typed(api.join_archive_request(user)["messages"], "ARCHIVE_JOIN")
 
-    lobby1 = KGS.get_typed(api.room_load_game(arh_join["games"][-1]["timestamp"], 22)["messages"], "GAME_JOIN")
-    lobby2 = KGS.get_typed(api.room_load_game(arh_join["games"][-2]["timestamp"], 22)["messages"], "GAME_JOIN")
+        lobby1 = KGS.get_typed(
+            api.room_load_game(arh_join["games"][-1]["timestamp"], 22)["messages"],
+            "GAME_JOIN")
+        lobby2 = KGS.get_typed(
+            api.room_load_game(arh_join["games"][-2]["timestamp"], 22)["messages"],
+            "GAME_JOIN")
+
+        return jsonify({
+            '1_game_user_color': KGS.get_colors(arh_join, -1)[0],
+            '1_game_user_score': KGS.get_score(arh_join, -1),
+            '1_game_user_duration': KGS.get_duration(lobby1),
+            '1_game_user_enemy_name': KGS.get_players(arh_join, -1)[1],
+            '1_game_user_enemy_color': KGS.get_colors(arh_join, -1)[1],
+            '1_game_user_enemy_score': KGS.get_score(arh_join, -1),
+            '2_game_user_color': KGS.get_colors(arh_join, -2)[0],
+            '2_game_user_score': KGS.get_score(arh_join, -2),
+            '2_game_user_duration': KGS.get_duration(lobby2),
+            '2_game_user_enemy_name': KGS.get_players(arh_join, -2)[1],
+            '2_game_user_enemy_color': KGS.get_colors(arh_join, -2)[1],
+            '2_game_user_enemy_score': KGS.get_score(arh_join, -2)
+        }
+        )
 
 
-    return jsonify({
-        '1_game_user_color': KGS.get_colors(arh_join, -1)[0],
-        '1_game_user_score': KGS.get_score(arh_join, -1),
-        '1_game_user_duration': KGS.get_duration(lobby1),
-        '1_game_user_enemy_name': KGS.get_players(arh_join, -1)[1],
-        '1_game_user_enemy_color': KGS.get_colors(arh_join, -1)[1],
-        '1_game_user_enemy_score': KGS.get_score(arh_join, -1),
-        '2_game_user_color': KGS.get_colors(arh_join, -2)[0],
-        '2_game_user_score': KGS.get_score(arh_join, -2),
-        '2_game_user_duration': KGS.get_duration(lobby2),
-        '2_game_user_enemy_name': KGS.get_players(arh_join, -2)[1],
-        '2_game_user_enemy_color': KGS.get_colors(arh_join, -2)[1],
-        '2_game_user_enemy_score': KGS.get_score(arh_join, -2)
-    }
-    )
 if __name__ == '__main__':
     app.run(port=80, host='127.0.0.1')
